c623bba1daa20aab0383111f3bbd9b968f234a90
Sandeep Mistry <s.mistry@arduino.cc>
2019-05-13 14:55:17 -0400

    GSMSSLClient: Remove "AmazonRootCA1" root certificate

--- src/GSMSSLClient.cpp
+++ src/GSMSSLClient.cpp
@@ -25,7 +25,8 @@
 
 enum {
   SSL_CLIENT_STATE_LOAD_ROOT_CERT,
-  SSL_CLIENT_STATE_WAIT_LOAD_ROOT_CERT_RESPONSE
+  SSL_CLIENT_STATE_WAIT_LOAD_ROOT_CERT_RESPONSE,
+  SSL_CLIENT_STATE_WAIT_DELETE_ROOT_CERT_RESPONSE
 };
 
 bool GSMSSLClient::_rootCertsLoaded = false;
@@ -55,18 +56,26 @@ int GSMSSLClient::ready()
 
   switch (_state) {
     case SSL_CLIENT_STATE_LOAD_ROOT_CERT: {
-      // load the next root cert
-      MODEM.sendf("AT+USECMNG=0,0,\"%s\",%d", GSM_ROOT_CERTS[_certIndex].name, GSM_ROOT_CERTS[_certIndex].size);
-      if (MODEM.waitForPrompt() != 1) {
-        // failure
-        ready = -1;
-      } else {
-        // send the cert contents
-        MODEM.write(GSM_ROOT_CERTS[_certIndex].data, GSM_ROOT_CERTS[_certIndex].size);
+      if (GSM_ROOT_CERTS[_certIndex].size) {
+        // load the next root cert
+        MODEM.sendf("AT+USECMNG=0,0,\"%s\",%d", GSM_ROOT_CERTS[_certIndex].name, GSM_ROOT_CERTS[_certIndex].size);
+        if (MODEM.waitForPrompt() != 1) {
+          // failure
+          ready = -1;
+        } else {
+          // send the cert contents
+          MODEM.write(GSM_ROOT_CERTS[_certIndex].data, GSM_ROOT_CERTS[_certIndex].size);
 
-        _state = SSL_CLIENT_STATE_WAIT_LOAD_ROOT_CERT_RESPONSE;
+          _state = SSL_CLIENT_STATE_WAIT_LOAD_ROOT_CERT_RESPONSE;
+          ready = 0;
+        }
+      } else {
+        // remove the next root cert name
+        MODEM.sendf("AT+USECMNG=2,0,\"%s\"", GSM_ROOT_CERTS[_certIndex].name);
+
+        _state = SSL_CLIENT_STATE_WAIT_DELETE_ROOT_CERT_RESPONSE;
         ready = 0;
-      } 
+      }
       break;
     }
 
@@ -86,6 +95,22 @@ int GSMSSLClient::ready()
 
         ready = 0;
       }
+      break;
+    }
+
+    case SSL_CLIENT_STATE_WAIT_DELETE_ROOT_CERT_RESPONSE: {
+      // ignore ready response, root cert might not exist
+      _certIndex++;
+
+      if (_certIndex == GSM_NUM_ROOT_CERTS) {
+        // all certs loaded
+        _rootCertsLoaded = true;
+      } else {
+        // load next
+        _state = SSL_CLIENT_STATE_LOAD_ROOT_CERT;
+      }
+
+      ready = 0;
       break;
     }
   }
